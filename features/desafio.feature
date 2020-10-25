0bc2f0459d40b77ea99e0b551cffdc89db79205c
deyvirson mendonca <deyvirsonbm@gmail.com>
2020-10-25 20:41:09 -0300

    adicionando primeiro validacao de escopo

--- features/desafio.feature
+++ features/desafio.feature
@@ -2,6 +2,7 @@
 
 Funcionalidade: Desafio Liferay
 
+@a
 Cenário: Validar que o texto "party rock" deve esta sendo exibido
     Dado que o usuário está na pagina principal
     Então visualiza o texto party rock
--- features/step_definitions/main_page_steps.rb
+++ features/step_definitions/main_page_steps.rb
@@ -2,3 +2,7 @@ Dado('que o usuário está na pagina principal') do
     @mp = MainPage.new
     @mp.open_main_page
 end
+
+Então('visualiza o texto party rock') do
+    expect(@mp.assert_rock_text).to eql("Let's party rock." )
+end
